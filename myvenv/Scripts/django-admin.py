7566c192a153344eeaf0ef4a7e10828dbb96d9b7
bbanderson <tombyun@naver.com>
2020-09-21 06:23:21 +0900

    Delete myvenv

--- myvenv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\user\OneDrive - 명지대학교\Likelion\중앙과제2-wordcount\myvenv\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
